1b94cdef79f50c59e5105ab18757fe280748d4db
MajinB10 <majinbhavesh@gmail.com>
2024-12-03 10:46:15 +0800

    Initial Commit

---
+++ HabitBuddy/HabitBuddyApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabitBuddyApp.swift
+//  HabitBuddy
+//
+//  Created by Bhavesh Anand on 3/12/24.
+//
+
+import SwiftUI
+
+@main
+struct HabitBuddyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ HabitBuddyTests/HabitBuddyTests.swift
@@ -0,0 +1,17 @@
+//
+//  HabitBuddyTests.swift
+//  HabitBuddyTests
+//
+//  Created by Bhavesh Anand on 3/12/24.
+//
+
+import Testing
+@testable import HabitBuddy
+
+struct HabitBuddyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
